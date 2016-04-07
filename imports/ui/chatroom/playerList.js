d80bac1621a90f858b48d30dfd3918625ef9c447
CyrusCai <Cyrus_Cai@epam.com>
2016-04-07 14:12:45 +0800

    show different color to murder/police

--- imports/ui/chatroom/playerList.js
+++ imports/ui/chatroom/playerList.js
@@ -8,18 +8,68 @@ Template.PlayerList.onCreated(function createPlayerlist(){
   this.autorun(() => {
     Meteor.subscribe('onlineusers');
   });
+
+  this.getRoomHolder = () => {
+    var holder = Meteor.users.findOne({ "status.online": true });
+    if(holder){
+      return holder.username;
+    }else{
+      return '';
+    };
+  };
+  this.getCurrentUserRole = () => {
+    debugger;
+    let chatroom = Chatrooms.findOne({});
+    let currentUser = Meteor.user();
+    if(chatroom){
+      let playerFind = chatroom.playerList.filter(function(player){
+        return player.username === currentUser.username;
+      });
+      return playerFind[0].role;
+    }else{
+      return '';
+    }
+  };
 });
 
 Template.PlayerList.helpers({
- participants() {
+ onlineUsers(){
   return Meteor.users.find({ "status.online": true }).fetch();
  },
- roomHolder(){
-  var holder = Meteor.users.findOne({ "status.online": true });
-  if(holder){
-    return holder;
-  }else{
-    return '';
+ participants() {
+  let chatroom = Chatrooms.findOne({});
+  if(chatroom){
+    return chatroom.playerList;
   }
+  },
+ gameStart(){
+  if(Chatrooms.findOne({})){
+    if(Chatrooms.findOne({}).roomStatus === 'Start')
+    return true;
+  }else{
+    return false;
+  }
+ },
+ isRoomHolder(username){
+  const instance = Template.instance();
+  if(username === instance.getRoomHolder()){
+    return true;
+  }else{
+    return false;
+  }
+ },
+ showMurder(){
+  const instance = Template.instance();
+  return instance.getCurrentUserRole() === 'murder';
+ },
+ showPolice(){
+  const instance = Template.instance();
+  return instance.getCurrentUserRole() === 'police';
+ },
+ isMurder(role){
+  return role === 'murder';
+ },
+ isPolice(role){
+  return role === 'police';
  }
 });
